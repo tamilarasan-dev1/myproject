f53cb31ab0637c5bbbe47a2fe07cc2fd31c790ce
tamil <tamilarasanpalanivel.in@gmail.com>
2026-04-01 17:18:49 +0530

    Refine Java demo

--- demo.java
+++ demo.java
@@ -1,6 +1,12 @@
-public class demo {
+public final class Demo {
+    private static final int ITERATION_COUNT = 10;
+
+    private Demo() {
+        // Prevent instantiation of this utility class.
+    }
+
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < ITERATION_COUNT; i++) {
             System.out.println("Iteration: " + i);
         }
     }
